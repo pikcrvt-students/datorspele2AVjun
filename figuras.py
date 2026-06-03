fd75f1ddb30d3a011240c9e6c4304c7963118fee
skyline3475 <sweard381@gmail.com>
2026-06-03 12:36:08 +0300

    pievienoju failus ar figuram

---
+++ figuras.py
@@ -0,0 +1,14 @@
+def figuras():
+    print("K - Karalis")
+    print("D - Dāma")
+    print("T - Tornis")
+    print("L - Laidnis")
+    print("Z - Zirgs")
+    print("P - Bandinieks")
+
+
+def noteikumi():
+    print("1. Šahu spēlē divi spēlētāji.")
+    print("2. Baltais spēlētājs sāk pirmais.")
+    print("3. Katrs spēlētājs izdara vienu gājienu.")
+    print("4. Mērķis ir dot pretinieka karalim matu.")
